60d456053a36efe4cd2159092e860cb4779a10bf
djkrose <djkrose@live.com>
2017-09-05 21:00:19 +0200

    Changed project file to .Net Framework 3.5 and removed non-essential projects from solution.

---
+++ Jint/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Resources;
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Jint for Unity")]
+[assembly: AssemblyDescription("Javascript interpreter for .NET which provides full ECMA 5.1 compliance. (Forked and modified for .Net Framework 3.5 and Unity 5.3)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sebastien Ros")]
+[assembly: AssemblyProduct("Jint")]
+[assembly: AssemblyCopyright("Copyright © Sébastien Ros")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("2.11.10.1")]
+[assembly: AssemblyFileVersion("2.11.10.1")]
